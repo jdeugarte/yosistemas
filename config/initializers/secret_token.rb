49a583af728fe55459444a51b749242219c2cf81
= <=>
2013-09-16 03:03:28 +0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yosistemas::Application.config.secret_key_base = '27a8d1f068bd76c0a993037136d4f5b3d6a6c72560158eb7d58521ba12531d7c49d8a931358d3214442878f64caa1e3d9c2aa335409af22a952ac0edacdc77c2'

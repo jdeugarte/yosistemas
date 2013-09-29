e7a1f1db7b8c01fbad82316486af1dec246bb78b
Andrea Quiroga Yankovic <andy.afqy@gmail.com>
2013-09-29 18:28:57 -0400

    spec basicos usuario

--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Usuario do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "tiene  un construtor valido" do
+    expect(FactoryGirl.build(:usuario)).to be_valid
+  end
+
+  it "no es valido si no tiene nombre" do
+    usuario = FactoryGirl.build(:usuario, nombre: nil)
+    expect(usuario).to have(1).errors_on(:nombre)
+    expect(usuario).to_not be_valid 
+  end
+  
+  it "no es valido si no tiene apellido" do
+    usuario = FactoryGirl.build(:usuario, nombre: 'andrea', apellido: nil)
+    expect(usuario).to have(1).errors_on(:apellido)
+    expect(usuario).to_not be_valid 
+  end
+  
+  it "no es valido si no tiene correo" do
+    usuario = FactoryGirl.build(:usuario, nombre: 'andrea', apellido: 'quiroga', correo: nil)
+    expect(usuario).to have(1).errors_on(:correo)
+    expect(usuario).to_not be_valid 
+  end
+  
+  
+  
 end

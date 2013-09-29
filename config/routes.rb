cac1ec5d8ba44ee27afa5b47bedc3bd6d136b5d8
Gary <garyantoriano@gmail.com>
2013-09-29 13:44:38 -0400

    Cambiando la pagina de inicio a Welcome#index y modificacion del navbar en el layout navigation

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Yosistemas::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root :to => 'sessions#new'
+ root :to => 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

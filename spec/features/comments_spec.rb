75406c0ebdaf8aa33e35b1f19067d92e54ce50f7
douglas campos <douglas14@hotmail.com>
2013-10-05 22:49:03 -0400

    agregada una prueba a tema y corregido feature spec de comentarios

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,18 +1,17 @@
 require 'spec_helper'
 
 feature 'Comentario' do
-=begin
+  def ingresar_sistema(usuario)
+    visit root_path
+    click_link "Ingresar"  
+    fill_in 'correo', with: 'email@email.com'
+    fill_in 'contrasenia', with: 'password'
+    click_button 'Ingresar'
+  end
   scenario 'Comentar tema' do
     #crear usuario que pueda crear una entrada (cuando se tenga usuarios)
     usuario = FactoryGirl.create(:usuario)
-    #ir a la ventana principal para iniciar la interaccion como lo haria el usuario)
-    visit root_path  
-    #si es necesario hacer un login, hacerlo (claro que podemos tener esto en un metodo en un helper, para no repetir en otros specs)
-    fill_in 'correo', with: 'email@email.com'
-    fill_in 'contrasenia', with: 'password'
-    click_button 'Log in'
-    #ahora si vamos a la seccion de temas
-    visit temas_path  # para mas info ver: guides.rubyonrails.org/routing.html
+    ingresar_sistema(usuario)
     click_link 'Nuevo Tema'
     expect{
       fill_in 'tema_titulo', with: 'Titulo tema de prueba'
@@ -20,16 +19,11 @@ feature 'Comentario' do
       click_button 'Crear tema'
     }.to change(Tema, :count).by(1)
     expect(current_path).to eq temas_path  #secion lista de temas
-    click_link 'Show' #voy al tema
+    visit "temas/"+Tema.first.id.to_s #voy al tema (esta parte necesita ser refactorizada)
     expect{
       fill_in 'comment_body', with: 'contenido del comentario'
-      click_button 'Submit'
-    }.to change(, :count).by(1)
-    expect(current_path).to eq Tema #lista de temas
-    expect(page).to have_content 'Titulo tema de prueba'
-    expect(page).to have_content 'Descripcion o contenido del tema de prueba'
-    expect(page).to have_content 'contenido del comentario'
-    #save_and_open_page #habilitar en cualquier lugar SOLO si se quiere hacer debug de este spec
+      click_button 'Comentar'
+    }.to change(Comment, :count).by(1)
+    expect(page).to have_content 'contenido del comentario' 
   end
-=end
 end
--- spec/features/temas_spec.rb
+++ spec/features/temas_spec.rb
@@ -27,4 +27,28 @@ feature 'Gestion de tema' do
     expect(page).to have_content 'Descripcion o contenido del tema de prueba'
   end
 
+   scenario 'Eliminar(dejar de mostrar) un tema' do
+    usuario = FactoryGirl.create(:usuario)
+
+    #Tarea para uds. refactorizar esta sección a un método ingresar_sistema(usuario)
+    #para no repetir esto en todos los demás feature specs que necesiten autentificarse
+    ingresar_sistema(usuario)
+    click_link 'Nuevo Tema'
+    
+    expect{
+      fill_in 'tema_titulo', with: 'Titulo tema de prueba'
+      fill_in 'tema_cuerpo', with: 'Descripcion o contenido del tema de prueba'
+      click_button 'Crear tema'
+    }.to change(Tema, :count).by(1)
+    expect(current_path).to eq temas_path  #no muy util aqui, pero sirve para mostrar esta opcion
+    expect(page).to have_content 'Titulo tema de prueba'
+    expect(page).to have_content 'Descripcion o contenido del tema de prueba'
+    visit "temas/"+Tema.first.id.to_s
+    click_link 'visible' # ya no quiero ver el tema
+    expect(current_path).to eq temas_path #lista de temas
+    expect(page).to have_content '' #no tengo contenido en la pagina
+    expect(page).to have_content ''
+  end
+
+
 end

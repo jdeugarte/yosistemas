3819658c3eb1fc9dbcf6cfcbad7b993a6d5202a3
armandoaliaga <armandoaliaga92@gmail.com>
2013-10-10 15:35:42 -0400

    haciendo gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def avatar_url(user)		
+    	gravatar_id = Digest::MD5::hexdigest(user.correo).downcase
+    	"http://gravatar.com/avatar/#{gravatar_id}.png?s=80&d=wavatar"
+  end
 end

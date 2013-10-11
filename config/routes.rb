4dcc11dee3ec9e5655c826988c6ac8ad861bfe16
armandoaliaga <armandoaliaga92@gmail.com>
2013-10-10 22:33:43 -0400

    moviendo la parte del login y haciendo el menu de configuraciones del usuario y arreglando algunos specs de feature que fallaban por los cambios realizados

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Yosistemas::Application.routes.draw do
   get "temas/index"
   post 'temas/edit/:id' => 'temas#edit', :as => 'edit_tema'
+  post 'sessions/create' => 'sessions#create', :as => 'loguear'
+  get 'sessions/destroy' => 'sessions#destroy', :as => 'desloguear'
   get "usuarios/confirm" => "usuarios#confirm"
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 feature 'Comentario' do
   def ingresar_sistema(usuario)
-    visit root_path
-    click_link "Ingresar"  
+    visit root_path    
+    #click_link "Ingresar"  
     fill_in 'correo', with: 'email@email.com'
     fill_in 'contrasenia', with: 'password'
     click_button 'Ingresar'

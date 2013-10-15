895754209fbc94e568dd77e042acf0046a5c20cb
armandoaliaga <armandoaliaga92@gmail.com>
2013-10-15 12:40:52 -0400

    haciendo la vista de ver grupos

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -2,13 +2,10 @@ require 'spec_helper'
 
 feature 'Gestion de Sessions' do
   scenario 'Log in de un usuario' do    
-    usuario = FactoryGirl.build(:usuario)  
-    #visit '/sessions/new'
+    usuario = FactoryGirl.build(:usuario)      
     visit root_path
     fill_in 'correo', with: 'email@email.com'
     fill_in 'contrasenia', with: 'password'
-    click_button 'Ingresar'
-    #expect(page).to have_text("Logged in!")
-    #expect(current_path).to eq temas_path
+    click_button 'Ingresar'   
   end
 end

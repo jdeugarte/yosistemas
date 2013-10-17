5d949bea663d4ffb7a2cb37a9ef7922259819ad8
garjoa <ags123123@gmail.com>
2013-10-17 13:16:00 -0400

    notificaciones en login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,31 @@ class SessionsController < ApplicationController
     usuario = Usuario.autenticar(params[:correo], params[:contrasenia])    
     if usuario
       session[:usuario_id] = usuario.id
+      @cont = check_notifications(usuario)
       if (request.referrer.include? "/usuarios/new") || (request.referrer.include? "usuarios/confirm?pass" ) || (request.referrer.include? "/send_password_mail")        
-        redirect_to root_url, :notice => "Logged in!"
+
+        redirect_to root_url, :notice => "Logged in!  "+@cont.to_s+"  notifications: "
       else
-        redirect_to :back, :notice => "Logged in!"
+        redirect_to :back, :notice => "Logged in!  "+@cont.to_s+"  notifications: "
       end
     else
         
       redirect_to :back, :notice => "Correo o Contrasenia Invalida!"      
     end
   end
+
+  def check_notifications(user)
+    @var = 0
+    @suscripciones = SuscripcionTema.where(usuario_id: user.id)
+    @suscripciones.each do |suscripcion|
+      @notificaciones = Notificacion.where(suscripcion_temas_id: suscripcion.id , notificado: false)
+      @notificaciones.each do |notificacion|
+        @var = @var+1
+      end
+    end
+    return @var
+  end
+
 
   def destroy
     session[:usuario_id] = nil

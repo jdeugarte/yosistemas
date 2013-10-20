e07c5088a7e32ed3a0d09e2e8100e18d4a0f0981
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2013-10-19 22:28:37 -0400

    subiendo pruebas de esta iteracion y mejoras de la enterior para controladores y models

--- spec/models/grupo_spec.rb
+++ spec/models/grupo_spec.rb
@@ -10,6 +10,21 @@ describe Grupo do
     expect(grupo).to have(1).errors_on(:nombre)
     expect(grupo).to_not be_valid 
   end
+
+  it "retorna false si NO corresponde a nombre" do
+    grupo = FactoryGirl.build(:grupo, nombre: "prueba")
+    expect(grupo.correspondeAGrupo("nombre")).to be_false
+  end
+  
+  it "retorna true si corresponde a nombre" do
+    grupo = FactoryGirl.build(:grupo, nombre: "prueba")
+    expect(grupo.correspondeAGrupo("pr")).to be_true
+  end
+  
+  it "retorna true si corresponde a nombre compuesto (dos o mas palabras de criterio de busqueda)" do
+    grupo = FactoryGirl.build(:grupo, nombre: "prueba")
+    expect(grupo.correspondeAGrupo("pr e")).to be_true
+  end
   
   
 end

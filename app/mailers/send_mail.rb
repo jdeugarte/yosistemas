9864fb06ff2f73f520de910210f5ce6f1cc6cb23
araoznelson <nelsonaraoz@ymail.com>
2013-10-20 23:08:14 -0400

    quitando if

--- app/mailers/send_mail.rb
+++ app/mailers/send_mail.rb
@@ -18,11 +18,7 @@ class SendMail < ActionMailer::Base
     @user = user
     @tema=tema
     @grupo=grupo
-    if @grupo.llave!= nil
-      @url  = 'http://yosistemas.herokuapp.com'
-    else
-        @url  = 'http://yosistemas.herokuapp.com/temas/'+tema.id.to_s
-    end
+    @url  = 'http://yosistemas.herokuapp.com/temas/'+tema.id.to_s
     mail(to: @user.correo,subject: 'YoSistemas comentario tema')
   end
   def notify_users_grupo(user,grupo_nombre)

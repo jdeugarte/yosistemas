dcb50f4f2359aad2f51803fc550d79518559b0bd
Israel Antezana R <israelantezana@gmail.com>
2013-10-30 08:38:59 -0400

    TODO: Refactorizaciones pendientes en feature specs, resultantes de clase miércoles 30/10

--- spec/features/temas_spec.rb
+++ spec/features/temas_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 feature 'Gestion de tema' do
+  #TODO: Llevar este método a un spec_helper para que sea usado por todos los feature specs
+  #TODO: Usar el parametro usuario en este método
   def ingresar_sistema(usuario)
     visit root_path
     #click_link "Ingresar"  
@@ -12,8 +14,6 @@ feature 'Gestion de tema' do
   scenario 'Crear un nuevo tema' do
     grupo = FactoryGirl.create(:grupo)
     usuario = FactoryGirl.create(:usuario)
-    #Tarea para uds. refactorizar esta sección a un método ingresar_sistema(usuario)
-    #para no repetir esto en todos los demás feature specs que necesiten autentificarse
     ingresar_sistema(usuario)
     click_link 'Nuevo Tema'
     
@@ -31,11 +31,12 @@ feature 'Gestion de tema' do
     grupo = FactoryGirl.create(:grupo)
     usuario = FactoryGirl.create(:usuario)
 
-    #Tarea para uds. refactorizar esta sección a un método ingresar_sistema(usuario)
+    #TODO: Tarea para uds. refactorizar esta sección a un método ingresar_sistema(usuario)
     #para no repetir esto en todos los demás feature specs que necesiten autentificarse
     ingresar_sistema(usuario)
     click_link 'Nuevo Tema'
     
+    #TODO: crear el tema con factory girl para eliminar este bloque 
     expect{
       fill_in 'tema_titulo', with: 'Titulo tema de prueba'
       fill_in 'tema_cuerpo', with: 'Descripcion o contenido del tema de prueba'
@@ -44,32 +45,35 @@ feature 'Gestion de tema' do
     expect(current_path).to eq temas_path  #no muy util aqui, pero sirve para mostrar esta opcion
     expect(page).to have_content 'Titulo tema de prueba'
     expect(page).to have_content 'Descripcion o contenido del tema de prueba'
-    visit "temas/"+Tema.first.id.to_s
+    #fin bloque 
+    visit "temas/"+Tema.first.id.to_s  #usar el id, titulo del tema con creado con factory girl
     click_link 'Dejar de mostrar tema' # ya no quiero ver el tema
     expect(current_path).to eq temas_path #lista de temas
     expect(page).to have_content '' #no tengo contenido en la pagina
     expect(page).to have_content ''
+    #TODO: to change(Tema, :count).by(1)  SE puede investigar si hay algo similar para reducir la cantidad en 1
   end
 
   scenario 'Ver mis temas' do
     grupo = FactoryGirl.create(:grupo)
     usuario = FactoryGirl.create(:usuario)
 
-    #Tarea para uds. refactorizar esta sección a un método ingresar_sistema(usuario)
-    #para no repetir esto en todos los demás feature specs que necesiten autentificarse
     ingresar_sistema(usuario)
     click_link 'Nuevo Tema'
-    
+
+    #TODO: Se puede remplazar este bloque usando factory girl??
     #creamos un tema para el usuario1
     fill_in 'tema_titulo', with: 'Titulo tema de prueba'
     fill_in 'tema_cuerpo', with: 'Descripcion o contenido del tema de prueba'
     click_button 'Crear tema'
+    #fin bloque
 
     click_link usuario.correo
     click_link 'Salir'
 
     usuario2 = FactoryGirl.create(:other_user)
 
+    #TODO: Usar el metodo de ingresar al sistema común que se debería encontrar en el helper
     #Ingresamos al sistema con otro usuario
     visit root_path 
     fill_in 'correo', with: 'email2@email.com'
@@ -116,6 +120,7 @@ end
 
 feature 'Buscar temas' do
 
+  ## TODO: Eliminar este método y usar el del helper definido arriba en este archivo
   def ingresar_sistema(usuario)
     visit root_path
     #click_link "Ingresar"  

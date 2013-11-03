00d58dc33c5f3bd817f0244ef3d2207152e84fcb
araoznelson <fke@hotmail.es>
2013-11-03 12:47:23 -0800

    Delete reponder_tareas.rb

--- app/models/reponder_tareas.rb
+++
@@ -1,7 +0,0 @@
-class ResponderTarea < ActiveRecord::Base
-  belongs_to :usuario
-  belongs_to :grupo
-  belong_to :tarea
-  has_many :archivo_adjuntos_respuesta
-	validates :descripcion, :presence => { :message => " es requerida" }
-end

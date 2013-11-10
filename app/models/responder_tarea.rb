68fc43467b1c35ca44474dc566512fed4c5f4188
araoznelson <nelsonaraoz@ymail.com>
2013-11-10 19:09:17 -0400

    refactorizando metodos de responder tarea

--- app/models/responder_tarea.rb
+++ app/models/responder_tarea.rb
@@ -4,4 +4,7 @@ class ResponderTarea < ActiveRecord::Base
   belongs_to :tarea
   has_many :archivo_adjunto_respuestas
   validates :descripcion, :presence => {:message => " es requerida"}
+  def self.ya_envio_tarea(usuario,tarea)
+  		ResponderTarea.where(:usuario_id => usuario,:tarea_id => tarea)
+  end
 end

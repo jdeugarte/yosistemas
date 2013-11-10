7e5e1322f968c2795bdd3d819e4d6fc63e6fb106
Diego R <diroag@gmail.com>
2013-11-09 20:53:07 -0400

    agregado spec para ver detalle de tarea

--- spec/factories/tareas.rb
+++ spec/factories/tareas.rb
@@ -2,8 +2,9 @@
 
 FactoryGirl.define do
   factory :tarea do
-    titulo "MyString"
-    descripcion "MyString"
+    titulo "Tarea de Prueba"
+    descripcion "Una tarea de prueba no muy larga para que no se resuma"
     fecha_entrega "2013-10-13"
+    hora_entrega "10:00 PM"
   end
 end
--- spec/features/tareas_spec.rb
+++ spec/features/tareas_spec.rb
@@ -22,6 +22,20 @@ feature 'Gestion de tareas' do
      expect(page).to have_content 'Titulo tarea de prueba'
      expect(page).to have_content 'Descripcion o contenido de tarea de prueb...'
    end
+
+   scenario 'Ver detalle de una tarea' do
+     usuario = FactoryGirl.create(:usuario)
+     grupo = FactoryGirl.create(:grupo, usuario_id: usuario.id)
+     subscripcion = FactoryGirl.create(:subscripcion, llave: "qwerty", usuario_id: usuario.id, grupo_id: grupo.id)
+     tarea = FactoryGirl.create(:tarea, usuario_id: usuario.id, grupo_id: grupo.id)
+     ingresar_sistema(usuario)
+     visit "/tareas/"+tarea.id.to_s
+     expect(page).to have_content 'pedrito' #deberia tener, el username del que creador de la tarea
+     expect(page).to have_content 'Tarea de Prueba' #deberia tener, el titulo de la tarea
+     expect(page).to have_content 'Una tarea de prueba no muy larga para que no se resuma' #deberia tener, la descripcion
+     expect(page).to have_content '13/Oct/2013' #deberia tener, la fecha de entrega
+     expect(page).to have_content '10:00 PM' #deberia tener, la hora de entrega
+  end
  
   # scenario 'Ver index de tareas de un grupo' do
   #   usuario = FactoryGirl.create(:usuario)

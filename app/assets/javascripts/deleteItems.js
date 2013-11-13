ed5cb03c6103a3fe3ae1c72c9c72dfdfcea5b5ed
araoznelson <nelsonaraoz@ymail.com>
2013-11-12 22:29:53 -0400

    Registrar usuario

--- app/assets/javascripts/deleteItems.js
+++
@@ -1,7 +0,0 @@
-function eliminarItemDeVista(elemId)
-  {
-    var element = document.getElementById(elemId);
-    element.parentNode.removeChild(element);
-    var hidden = document.getElementById("elemsParaElim");
-    hidden.value = hidden.value+"-"+elemId;
-  }

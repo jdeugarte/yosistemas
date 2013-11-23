6a93a68a9ace1848090a338c8b697b421ec41966
jheorge <jheorge@gmail.com>
2013-11-24 00:37:23 +0100

    corrigiendo errores en la migracion

--- db/migrate/20131105221337_eliminar_columna_de_contrasenia_de_confirmacion.rb
+++ db/migrate/20131105221337_eliminar_columna_de_contrasenia_de_confirmacion.rb
@@ -1,5 +1,11 @@
 class EliminarColumnaDeContraseniaDeConfirmacion < ActiveRecord::Migration
-  def down
-  	remove_column :usuarios, :contrasenia_de_confirmacion, :string
+  
+  def self.up
+    remove_column :usuarios, :contrasenia_de_confirmacion
   end
+  
+  def self.down
+    remove_column :usuarios, :contrasenia_de_confirmacion, :string
+  end
+ 
 end

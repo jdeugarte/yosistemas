025fdd8bc7199f750a25d6a89d68e55ee4e81dc9
jheorge <jheorge@gmail.com>
2013-11-24 01:24:41 +0100

    erro de migracion staging

--- db/migrate/20131105221337_eliminar_columna_de_contrasenia_de_confirmacion.rb
+++ db/migrate/20131105221337_eliminar_columna_de_contrasenia_de_confirmacion.rb
@@ -1,11 +1,5 @@
 class EliminarColumnaDeContraseniaDeConfirmacion < ActiveRecord::Migration
   
-  def self.up
-    remove_column :usuarios, :contrasenia_de_confirmacion
-  end
   
-  def self.down
-    remove_column :usuarios, :contrasenia_de_confirmacion, :string
-  end
  
 end

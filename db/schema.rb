a146966abda02fa67691968f78c8cd807cbc7897
SebastianGerard <ger.sal.jo@gmail.com>
2013-11-26 20:21:05 -0400

    Boton me gusta de fb

--- db/schema.rb
+++ db/schema.rb
@@ -228,11 +228,10 @@ ActiveRecord::Schema.define(version: 20131126192205) do
     t.string   "nombre"
     t.string   "apellido"
     t.string   "contrasenia"
-    t.string   "contrasenia_de_confirmacion"
     t.string   "correo"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "activa",                      default: false
+    t.boolean  "activa",                   default: false
     t.integer  "grupo_id"
     t.integer  "solicitud_contrasenia_id"
     t.string   "rol"

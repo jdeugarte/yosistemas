bbd140783219b6b598000c78bdee3029b683e6d3
Andrea Quiroga Yankovic <andy.afqy@gmail.com>
2013-12-01 20:08:01 -0400

    referencia

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201084906) do
+ActiveRecord::Schema.define(version: 20131201222858) do
 
   create_table "adjunto_tarea_comentarios", force: true do |t|
     t.integer  "tarea_comentario_id"
@@ -240,6 +240,7 @@ ActiveRecord::Schema.define(version: 20131201084906) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.time     "hora_entrega"
+    t.integer  "tarea_base"
   end
 
   add_index "tareas", ["grupo_id"], name: "index_tareas_on_grupo_id"
@@ -273,16 +274,17 @@ ActiveRecord::Schema.define(version: 20131201084906) do
     t.string   "nombre"
     t.string   "apellido"
     t.string   "contrasenia"
+    t.string   "contrasenia_de_confirmacion"
     t.string   "correo"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "activa",                   default: false
+    t.boolean  "activa",                      default: false
     t.integer  "grupo_id"
     t.integer  "solicitud_contrasenia_id"
     t.string   "rol"
     t.string   "nombre_usuario"
     t.boolean  "mostrar_correo"
-    t.boolean  "conectado",                default: false
+    t.boolean  "conectado",                   default: false
   end
 
   add_index "usuarios", ["grupo_id"], name: "index_usuarios_on_grupo_id"

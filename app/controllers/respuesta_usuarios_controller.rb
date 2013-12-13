84625f6d3a57f3a5b30d5b851eaf0273c8cc1d46
Andrea Quiroga Yankovic <andy.afqy@gmail.com>
2013-12-13 17:34:15 -0400

    responder cuestionario solo suscritos no el creador

--- app/controllers/respuesta_usuarios_controller.rb
+++ app/controllers/respuesta_usuarios_controller.rb
@@ -6,11 +6,12 @@ class RespuestaUsuariosController < ApplicationController
 		#@cuestionario_id = params[:id]
 	end
 
-	def crear
+	def crear 
 		@respuestas = params[:resp]
 		@cuestionario_id = params[:id_cuestionario]
 		@usuario_id = params[:id_usuario]
 		@preguntas_id = params[:id_pregunta]
+		
 		cont = 0
 		@respuestas.each do |r|
 			@respuestaUsuario = RespuestaUsuario.new(:respuesta => r, :cuestionario_id => @cuestionario_id , 

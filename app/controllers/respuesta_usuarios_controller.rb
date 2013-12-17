76ca5935472e1c01aeaa5f31959ea02c155af10b
Gary <garyantoriano@gmail.com>
2013-12-17 16:55:23 -0400

    correccion de problemas al responder cuestionario

--- app/controllers/respuesta_usuarios_controller.rb
+++ app/controllers/respuesta_usuarios_controller.rb
@@ -7,31 +7,35 @@ class RespuestaUsuariosController < ApplicationController
 		#@cuestionario_id = params[:id]
 	end
 	def crear 
-		
 		@respuestas = params[:resp]
 		@cuestionario_id = params[:id_cuestionario]
 		@usuario_id = params[:id_usuario]
 		@preguntas_id = params[:id_pregunta]
 		@pregunta_tipo = params[:tipo_pregunta]
 		cont = 0
+
 		if(!RespuestaUsuario.ya_respondio_cuestionario(current_user.id,@cuestionario_id))
-			@respuestas.each do |r|
-				@respuestaUsuario = RespuestaUsuario.new(:respuesta => r, :cuestionario_id => @cuestionario_id , 
-					:usuario_id => @usuario_id, :pregunta_id => @preguntas_id[cont], :tipo => @pregunta_tipo[cont])
-				res = Respuesta.where(pregunta_id: @preguntas_id[cont])
-				res.each do |reee|
-					if @preguntas_id[cont].to_s==reee.pregunta_id.to_s && r==reee.texto && reee.respuesta_correcta == true
-						@respuestaUsuario.calificacion = true
-						break
-					else
-						@respuestaUsuario.calificacion = false
-					end
-				end	   
-				@respuestaUsuario.save
-				#agregar_archivos_adjuntos(@respuestaUsuario.id)
-				cont += 1
-			end
+			@respuestas.each { |key,respuesta_usuario|  
+				respuesta_usuario.each { |valor|
+					@respuestaUsuario = RespuestaUsuario.new(:respuesta => valor, :cuestionario_id => @cuestionario_id , 
+						:usuario_id => @usuario_id, :pregunta_id => key.to_s, :tipo => @pregunta_tipo[key].to_s)
+
+					res = Respuesta.where(pregunta_id: key)
+					res.each do |reee|
+						if key.to_s==reee.pregunta_id.to_s && valor==reee.texto && reee.respuesta_correcta == true
+							@respuestaUsuario.calificacion = true
+							break
+						else
+							@respuestaUsuario.calificacion = false
+						end
+					end	
+					@respuestaUsuario.save
+				}
+			}
 		end
+
+
+		
 	end
 =begin
 	def crear

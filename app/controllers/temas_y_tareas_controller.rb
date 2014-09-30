8e8e3e0d21b36504ee220bcef39a00ee9c8acad8
andresjordanze <andresjordanze@gmail.com>
2014-09-30 18:23:52 -0400

    css de diferencia entre grupos publicos y privados

--- app/controllers/temas_y_tareas_controller.rb
+++ app/controllers/temas_y_tareas_controller.rb
@@ -2,18 +2,18 @@ class TemasYTareasController < ApplicationController
   skip_before_filter :require_log_in,:only=>[:index]
   def index
     
-    if( params[:id] != nil && Grupo.find(params[:id]).habilitado )
+    if( params[:id] != nil && Grupo.find(params[:id]).habilitado)
        @grupo = Grupo.find(params[:id])
      else
        @grupo = Grupo.find(1)
        redirect_to temas_path
     end
-   if(params[:id]=="1")
-    redirect_to temas_path
-   else
-    @temas_y_tareas = @grupo.temas.where("visible = ?", 1) + @grupo.tareas
-    @temas_y_tareas.sort!{|x,y|  y.created_at <=> x.created_at}
-   end
+    if(params[:id]=="1")
+      redirect_to temas_path
+    else
+      @temas_y_tareas = @grupo.temas.where("visible = ?", 1)+@grupo.tareas
+      @temas_y_tareas.sort!{|x,y|  y.created_at <=> x.created_at}
+    end
   end
 
 end

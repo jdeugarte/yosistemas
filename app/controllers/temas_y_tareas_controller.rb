5a975a5305d68c5e8336d08a4d61e4bc1a8ba68d
andresjordanze <andresjordanze@gmail.com>
2014-09-30 21:56:36 -0400

    paginacion de temas y tareas

--- app/controllers/temas_y_tareas_controller.rb
+++ app/controllers/temas_y_tareas_controller.rb
@@ -4,15 +4,16 @@ class TemasYTareasController < ApplicationController
     
     if( params[:id] != nil && Grupo.find(params[:id]).habilitado)
        @grupo = Grupo.find(params[:id])
-     else
+    else
        @grupo = Grupo.find(1)
        redirect_to temas_path
     end
     if(params[:id]=="1")
       redirect_to temas_path
     else
-      @temas_y_tareas = @grupo.temas.where("visible = ?", 1)+@grupo.tareas
-      @temas_y_tareas.sort!{|x,y|  y.created_at <=> x.created_at}
+      @temas = @grupo.temas.order("updated_at DESC").page(params[:page]).per(3)
+      @tareas = Tarea.where(:grupo_id => params[:id]).order("updated_at DESC").page(params[:page]).per(3)
     end
   end
+
 end

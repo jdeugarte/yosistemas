e10b1eda9b91087ac042acd667ff930c22d736b8
MartinArispeRiveros <martinarisperiveros@gmail.com>
2014-10-22 01:33:18 -0400

    Tareas en menu principal

--- app/controllers/temas_y_tareas_controller.rb
+++ app/controllers/temas_y_tareas_controller.rb
@@ -13,7 +13,35 @@ class TemasYTareasController < ApplicationController
     else
       @temas = @grupo.temas.order("updated_at DESC").page(params[:page]).per(3)
       @tareas = Tarea.where(:grupo_id => params[:id]).order("updated_at DESC").page(params[:page]).per(3)
+      @all = @temas+@tareas.order("updated_at DESC").page(params[:page]).per(6)
     end
   end
+
+  def buscar
+    @temas = Array.new
+    @grupo = Grupo.find(params[:grupo])
+    aux = Tema.where(:grupo_id=>params[:grupo])
+    if params[:search] != "" && params[:search] != nil
+      byAll = Tema.allResultsSearchs(params[:search])
+      @temas = byAll        
+      @temas.each do |tema|
+        if !tema.grupo.habilitado
+          @temas.delete(tema)
+        end
+      end          
+    end
+    @ides=sacarIds(@temas)
+    @temas= Kaminari.paginate_array(@temas).page(params[:page]).per(5)
+    render 'index'
+  end
+
+  def sacarIds(temas)
+    concatenacion=""
+    temas.each do |tema|
+      concatenacion=concatenacion+"-"+tema.id.to_s
+    end
+    return concatenacion
+  end
+
 
 end

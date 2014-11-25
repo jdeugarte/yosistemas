8b17234f9af69d4e046423fb8a645368549e3c61
sergiokv13 <sergiokv13@sergiokv13.(none)>
2014-11-24 20:28:10 -0400

    chat e inbox funcionando

--- app/assets/javascripts/chat-scripts.js
+++ app/assets/javascripts/chat-scripts.js
@@ -1,89 +1,78 @@
- var timer;
-  function send()
-    {
-
-      var para_u=document.getElementById('chat_name').innerHTML;
-      var mensaje_u=document.getElementById("mensaje_sincrono").value;
-      if(mensaje_u)
-      {
-        var usuario_id_u=document.getElementById('id_usuario_envio').value;
-        $.post( "/mensajes/enviar", { para: para_u, mensaje: mensaje_u } );
-        document.getElementById("mensaje_sincrono").value="";
-        document.getElementById('chat_conversacion').innerHTML+="<div class='row'><div class='col-lg-12'><div class='pull-right pager' style='margin-bottom: 5px; margin-top: 0px;'><ul><li><a style='color:white;  background-color:#428BCA;' title='"+mensaje_u+"'>"+mensaje_u+"</a></li></ul></div></div></div>";
-        document.getElementById('chat_conversacion').scrollTop = 99999999;     
-      }
-    }
-      function showChat(nombre_usuario,usuario_id)
-      {
-        document.getElementById('min_chat_container').innerHTML = "no";
-        document.getElementById('minimizeChat').style.display = 'none';
-        var aux = 'User' + nombre_usuario;
-        document.getElementById('chat_conversacion').innerHTML="";
-        //document.getElementById('chat_name').innerHTML = nombre_usuario;
-        document.getElementById('chat_name').innerHTML = "<a href='/usuarios/"+usuario_id+"'>"+nombre_usuario+"</a>";
-        document.getElementById('chat_minimized_name').innerHTML=nombre_usuario;
-        document.getElementById("id_usuario_envio").value=usuario_id;
-        $("#chat_window").addClass("in");
-        $("#chat_window").show();        
-         $.get('/usuarios/obtener_charla/'+usuario_id, function(response){          
-
-            response=response.replace(/\&quot;/g, '"');
-            var conversacion=JSON.parse(response)
-            for(var i=0;i<conversacion.length;i++)
-            {              
-              if(conversacion[i].para_usuario_id!=usuario_id)
-              {
-                document.getElementById('chat_conversacion').innerHTML+="<div class='row'><div class='col-lg-12'><div class='pull-left pager' style='margin-bottom: 5px; margin-top: 0px;'><ul style='padding-left:0px;'><li><a style='color:black;' title='"+conversacion[i].created_at+"'>"+conversacion[i].mensaje+"</a></li></ul></div></div></div>";
-              }
-              else
-              {
-
-
-                document.getElementById('chat_conversacion').innerHTML+="<div class='row'><div class='col-lg-12'><div class='pull-right pager' style='margin-bottom: 5px; margin-top: 0px;'><ul><li><a style='color:white;  background-color:#428BCA;' title='"+conversacion[i].created_at+"'>"+conversacion[i].mensaje+"</a></li></ul></div></div></div>"; 
-              }    
-            } 
-             document.getElementById('chat_conversacion').scrollTop = 99999999;           
-          });
-       
-      }
-      function closeChat()
-      {
-         $("#chat_window").removeClass("in");
-         $("#chat_window").hide();
-      }
-
-     function showChatAux()
-    {
-      var usuario =document.getElementById('chat_name').innerHTML;
-      var usuario_id=document.getElementById('id_usuario_envio').value;
-      showChat(usuario,usuario_id);
-    }
-
-    function hideChat()
-    {
-      var usuario =document.getElementById('chat_name').innerHTML;
-      $("#chat_window").hide();
-      //document.getElementById('min_chat_container').innerHTML+='<div id="min_chat_container"> <div id="User' + usuario + '"> <a href="#" onclick="showChatAux()" >'+ usuario +'</a>  </div> </div>';
-      document.getElementById('minimizeChat').style.display = 'inline';
-      document.getElementById('min_chat_container').innerHTML = "minimized";
-      //blinkMinimizedChat();
-    }
-
-    function closeMinimizeChat()
-    {
-       document.getElementById('minimizeChat').style.display = 'none';
-    }
-
-    function blinkMinimizedChat()
-    {
-      timer = setInterval(blink, 10);
-        function blink()
-        {
-          $("#chat_minimized_name").fadeTo(100, 0.1).fadeTo(1000, 1.0);
-        }
-    }
-
-    function dropBlinking()
-    {
-      clearInterval(timer);
-    }y
+var timer;
+function send()
+{
+var para_u=document.getElementById('chat_name').innerHTML;
+var mensaje_u=document.getElementById("mensaje_sincrono").value;
+if(mensaje_u)
+{
+var usuario_id_u=document.getElementById('id_usuario_envio').value;
+$.post( "/mensajes/enviar", { para: para_u, mensaje: mensaje_u } );
+document.getElementById("mensaje_sincrono").value="";
+document.getElementById('chat_conversacion').innerHTML+="<div class='row'><div class='col-lg-12'><div class='pull-right pager' style='margin-bottom: 5px; margin-top: 0px;'><ul><li><a style='color:white; background-color:#428BCA;' title='"+mensaje_u+"'>"+mensaje_u+"</a></li></ul></div></div></div>";
+document.getElementById('chat_conversacion').scrollTop = 99999999;
+}
+}
+function showChat(nombre_usuario,usuario_id)
+{
+document.getElementById('min_chat_container').innerHTML = "no";
+document.getElementById('minimizeChat').style.display = 'none';
+var aux = 'User' + nombre_usuario;
+document.getElementById('chat_conversacion').innerHTML="";
+document.getElementById('chat_name').innerHTML=nombre_usuario;
+document.getElementById('chat_minimized_name').innerHTML=nombre_usuario;
+document.getElementById("id_usuario_envio").value=usuario_id;
+$("#chat_window").addClass("in");
+$("#chat_window").show();
+$.get('/usuarios/obtener_charla/'+usuario_id, function(response){
+response=response.replace(/\&quot;/g, '"');
+var conversacion=JSON.parse(response)
+for(var i=0;i<conversacion.length;i++)
+{
+if(conversacion[i].para_usuario_id!=usuario_id)
+{
+document.getElementById('chat_conversacion').innerHTML+="<div class='row'><div class='col-lg-12'><div class='pull-left pager' style='margin-bottom: 5px; margin-top: 0px;'><ul style='padding-left:0px;'><li><a style='color:black;' title='"+conversacion[i].created_at+"'>"+conversacion[i].mensaje+"</a></li></ul></div></div></div>";
+}
+else
+{
+document.getElementById('chat_conversacion').innerHTML+="<div class='row'><div class='col-lg-12'><div class='pull-right pager' style='margin-bottom: 5px; margin-top: 0px;'><ul><li><a style='color:white; background-color:#428BCA;' title='"+conversacion[i].created_at+"'>"+conversacion[i].mensaje+"</a></li></ul></div></div></div>";
+}
+}
+document.getElementById('chat_conversacion').scrollTop = 99999999;
+});
+}
+function closeChat()
+{
+$("#chat_window").removeClass("in");
+$("#chat_window").hide();
+}
+function showChatAux()
+{
+var usuario =document.getElementById('chat_name').innerHTML;
+var usuario_id=document.getElementById('id_usuario_envio').value;
+showChat(usuario,usuario_id);
+}
+function hideChat()
+{
+var usuario =document.getElementById('chat_name').innerHTML;
+$("#chat_window").hide();
+//document.getElementById('min_chat_container').innerHTML+='<div id="min_chat_container"> <div id="User' + usuario + '"> <a href="#" onclick="showChatAux()" >'+ usuario +'</a> </div> </div>';
+document.getElementById('minimizeChat').style.display = 'inline';
+document.getElementById('min_chat_container').innerHTML = "minimized";
+//blinkMinimizedChat();
+}
+function closeMinimizeChat()
+{
+document.getElementById('minimizeChat').style.display = 'none';
+}
+function blinkMinimizedChat()
+{
+timer = setInterval(blink, 10);
+function blink()
+{
+$("#chat_minimized_name").fadeTo(100, 0.1).fadeTo(1000, 1.0);
+}
+}
+function dropBlinking()
+{
+clearInterval(timer);
+}

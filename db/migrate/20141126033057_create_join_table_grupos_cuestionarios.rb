ec9c082557bd3f88338ba1f44ab4c508fcd33f49
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2014-11-26 07:17:01 -0400

    Subiendo

--- db/migrate/20141126033057_create_join_table_grupos_cuestionarios.rb
+++ db/migrate/20141126033057_create_join_table_grupos_cuestionarios.rb
@@ -3,19 +3,4 @@ class CreateJoinTableGruposCuestionarios < ActiveRecord::Migration
       t.integer :cuestionario_id
       t.integer :grupo_id
     end
-    
-    create_table :grupos_temas, id: false do |t|
-      t.integer :grupo_id
-      t.integer :tema_id
-    end
-
-    create_table :grupos_tareas, id: false do |t|
-      t.integer :grupo_id
-      t.integer :tarea_id
-    end
-
-    create_table :eventos_grupos, id: false do |t|
-      t.integer :grupo_id
-      t.integer :evento_id
-    end
 end

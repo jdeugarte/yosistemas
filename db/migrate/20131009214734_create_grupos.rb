99b1e56cf4e5b726c15dde9f6c459fa8fa43e245
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2014-11-26 07:13:30 -0400

    Migration 2

--- db/migrate/20131009214734_create_grupos.rb
+++ db/migrate/20131009214734_create_grupos.rb
@@ -10,22 +10,5 @@ class CreateGrupos < ActiveRecord::Migration
 
       t.timestamps
     end
-
-    create_table :grupos_temas, id: false do |t|
-      t.integer :grupo_id
-      t.integer :tema_id
-    end
-
-    create_table :grupos_tareas, id: false do |t|
-      t.integer :grupo_id
-      t.integer :tarea_id
-    end
-
-    create_table :eventos_grupos, id: false do |t|
-      t.integer :grupo_id
-      t.integer :evento_id
-    end
-
-    
   end
 end
